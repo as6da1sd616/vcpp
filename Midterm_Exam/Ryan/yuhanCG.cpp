a939cf1e8d3d0481e4840c1cc3c1c8ba40d8c659
unknown <znakehfdl@gmail.com>
2023-11-13 19:35:51 +0900

    commit message

---
+++ Midterm_Exam/Ryan/yuhanCG.cpp
@@ -0,0 +1,168 @@
+#include "yuhanCG.h"
+ void DrawBonobono(HWND hWnd, HDC hdc, int blink) {
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    bool sp;
+    sp = blink;
+    
+        // 박스 그리기
+        HBRUSH hBrush = CreateSolidBrush(RGB(127, 200, 255)); // 파랑 
+        SelectObject(hdc, hBrush);
+        Ellipse(hdc, 250, 100, 550, 400);
+
+        HBRUSH h3Brush = CreateSolidBrush(RGB(255, 150, 255)); //핑크
+        SelectObject(hdc, h3Brush);
+        Ellipse(hdc, 385, 240, 415, 310);
+        DeleteObject(hBrush);
+
+        HBRUSH h2Brush = CreateSolidBrush(RGB(255, 255, 255)); // 흰색
+        SelectObject(hdc, h2Brush);
+        Ellipse(hdc, 400, 245, 440, 275);
+        Ellipse(hdc, 360, 245, 400, 275);
+        HBRUSH h1Brush = CreateSolidBrush(RGB(0, 0, 0)); // 검정
+        SelectObject(hdc, h1Brush);
+        Ellipse(hdc, 380, 225, 420, 260);
+        DeleteObject(hBrush);
+
+        //수염
+        // 시작점 설정
+        MoveToEx(hdc, 340, 250, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 380, 260);
+        // 시작점 설정
+        MoveToEx(hdc, 340, 270, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 380, 265);
+
+        // 시작점 설정
+        MoveToEx(hdc, 460, 250, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 420, 260);
+        // 시작점 설정
+        MoveToEx(hdc, 460, 270, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 420, 265);
+
+        if (sp == false) {
+            //일반 눈깔 
+            Ellipse(hdc, 300, 190, 310, 205);
+            Ellipse(hdc, 470, 190, 480, 205);
+            SelectObject(hdc, h2Brush);
+            Ellipse(hdc, 302, 195, 308, 200);
+            Ellipse(hdc, 472, 195, 479, 200);
+        }
+        else {
+            SelectObject(hdc, h1Brush);
+            // >< 눈깔 
+                // 시작점 설정
+            MoveToEx(hdc, 300, 190, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 320, 205);
+            // 시작점 설정
+            MoveToEx(hdc, 300, 210, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 320, 205);
+
+            // 시작점 설정
+             // 시작점 설정
+            MoveToEx(hdc, 470, 205, NULL);
+            LineTo(hdc, 500, 190);
+            MoveToEx(hdc, 470, 205, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 500, 210);
+        }
+
+    
+
+}
+
+
+
+
+
+void DrawRyan(HWND hWnd, HDC hdc, int left, int top, int right, int bottom) {
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    int x = left;
+    int y = top;
+    int ex = right;
+    int ey = bottom;
+        // 박스 그리기
+        HBRUSH hBrush = CreateSolidBrush(RGB(255, 200, 15)); // 주황
+        SelectObject(hdc, hBrush);
+        int a, b, c, d;
+        a = x;
+        b = y;
+        c = ex;
+        d = ey;
+
+        if (x > ex) { //뒤집힘 방지 
+            a = x;
+            x = ex;
+            ex = a;
+        }
+        if (y > ey) {
+            b = y;
+            y = ey;
+            ey = b;
+        }
+
+        if (x!=0&&x <= 24) {  //드로잉영역 쳐나가기 방지
+            x = 24;
+        }
+        if (y!=0&&y <= 104) {
+            y = 104;
+        }
+        if (ex >= 768) {
+            ex = 768;
+        }
+        if (ey >= 440) {
+            ey = 440;
+        }
+
+
+
+        int resultx = ex - x; // x길이 
+        int resulty = ey - y; // y길이
+        //Rectangle(hdc, x, y, ex, ey); 
+
+        Ellipse(hdc, x, y, x + resultx * 0.27, y + resulty * 0.27); //왼쪽귀 
+
+        Ellipse(hdc, x + resultx * 0.725, y, ex, y + resulty * 0.27);// 오른쪽귀
+        Ellipse(hdc, x, y, ex, ey);//면상
+
+
+
+        HBRUSH h2Brush = CreateSolidBrush(RGB(255, 255, 255)); // 흰색
+        SelectObject(hdc, h2Brush);
+        Ellipse(hdc, x + resultx * 0.51, y + resulty * 0.5, x + resultx * 0.67, y + resulty * 0.62); //입
+        Ellipse(hdc, x + resultx * 0.35, y + resulty * 0.5, x + resultx * 0.51, y + resulty * 0.62); //입
+        HBRUSH h1Brush = CreateSolidBrush(RGB(0, 0, 0)); // 검정
+        SelectObject(hdc, h1Brush);
+        DeleteObject(hBrush);
+
+        //눈썹
+        MoveToEx(hdc, x + resultx * 0.16, y + resulty * 0.29, NULL);
+        LineTo(hdc, x + resultx * 0.35, y + resulty * 0.29);
+        MoveToEx(hdc, x + resultx * 0.16, y + resulty * 0.29 + 1, NULL);
+        LineTo(hdc, x + resultx * 0.35, y + resulty * 0.29 + 1);
+        MoveToEx(hdc, x + resultx * 0.16, y + resulty * 0.29 + 2, NULL);
+        LineTo(hdc, x + resultx * 0.35, y + resulty * 0.29 + 2);
+
+
+        MoveToEx(hdc, x + resultx * 0.62, y + resulty * 0.29, NULL);
+        LineTo(hdc, x + resultx * 0.82, y + resulty * 0.29);
+        MoveToEx(hdc, x + resultx * 0.62, y + resulty * 0.29 + 1, NULL);
+        LineTo(hdc, x + resultx * 0.82, y + resulty * 0.29 + 1);
+        MoveToEx(hdc, x + resultx * 0.62, y + resulty * 0.29 + 2, NULL);
+        LineTo(hdc, x + resultx * 0.82, y + resulty * 0.29 + 2);
+
+        //일반 눈깔 
+        Ellipse(hdc, x + resultx * 0.23, y + resulty * 0.36, x + resultx * 0.29, y + resulty * 0.42);
+        Ellipse(hdc, x + resultx * 0.7, y + resulty * 0.36, x + resultx * 0.76, y + resulty * 0.42);
+
+
+
+    
+
+}
---
+++ Midterm_Exam/bonobono/Bonobono.cpp
@@ -0,0 +1,282 @@
+﻿#include <windows.h>
+
+POINT drawstart, drawend; //드로잉 영역 좌표 
+BOOL mindraw = FALSE; // 마우스가 드로잉 영역에 있는가 
+int drawmode = 0;
+int endX, endY;
+// 이전 사각형의 좌표를 저장하는 변수
+int prevStartX, prevStartY, prevEndX, prevEndY, pStartX, pStartY, pEndX, pEndY;
+POINT startPos;
+BOOL isDrawing = FALSE;
+bool sp = false;
+bool bonoVisible = false;
+
+// 보노보노  그리기 함수
+void bono(HWND hWnd, HDC hdc,bool sp) {
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+
+    if (bonoVisible) {
+        // 박스 그리기
+        HBRUSH hBrush = CreateSolidBrush(RGB(127, 200, 255)); // 파랑 
+        SelectObject(hdc, hBrush);
+        Ellipse(hdc, 250, 100, 550, 400);
+
+        HBRUSH h3Brush = CreateSolidBrush(RGB(255, 150, 255)); //핑크
+        SelectObject(hdc, h3Brush);
+        Ellipse(hdc, 385, 240, 415, 310);
+        DeleteObject(hBrush);
+
+        HBRUSH h2Brush = CreateSolidBrush(RGB(255, 255, 255)); // 흰색
+        SelectObject(hdc, h2Brush);
+        Ellipse(hdc, 400, 245, 440, 275);
+        Ellipse(hdc, 360, 245, 400, 275);
+        HBRUSH h1Brush = CreateSolidBrush(RGB(0, 0, 0)); // 검정
+        SelectObject(hdc, h1Brush);
+        Ellipse(hdc, 380, 225, 420, 260);
+        DeleteObject(hBrush);
+
+        //수염
+        // 시작점 설정
+        MoveToEx(hdc, 340, 250, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 380, 260);
+        // 시작점 설정
+        MoveToEx(hdc, 340, 270, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 380, 265);
+
+        // 시작점 설정
+        MoveToEx(hdc, 460, 250, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 420, 260);
+        // 시작점 설정
+        MoveToEx(hdc, 460, 270, NULL);
+        // 직선의 끝점 설정
+        LineTo(hdc, 420, 265);
+
+        if (sp == false) {
+            //일반 눈깔 
+            Ellipse(hdc, 300, 190, 310, 205);
+            Ellipse(hdc, 470, 190, 480, 205);
+            SelectObject(hdc, h2Brush);
+            Ellipse(hdc, 302, 195, 308, 200);
+            Ellipse(hdc, 472, 195, 479, 200);
+        }
+        else {
+            SelectObject(hdc, h1Brush);
+            // >< 눈깔 
+                // 시작점 설정
+            MoveToEx(hdc, 300, 190, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 320, 205);
+            // 시작점 설정
+            MoveToEx(hdc, 300, 210, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 320, 205);
+
+            // 시작점 설정
+             // 시작점 설정
+            MoveToEx(hdc, 470, 205, NULL);
+            LineTo(hdc, 500, 190);
+            MoveToEx(hdc, 470, 205, NULL);
+            // 직선의 끝점 설정
+            LineTo(hdc, 500, 210);
+        }
+
+    }
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_KEYDOWN:
+        if ((wParam == VK_SPACE)) {
+
+            sp = true;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break; //이걸 왜빼먹어서 안되게하냐 
+    case WM_KEYUP:
+        if ((wParam == VK_SPACE)) {
+
+            sp = false;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+    case WM_COMMAND:
+        if (LOWORD(wParam) == 1) { //1번 
+            drawmode = 1;
+
+            InvalidateRect(hWnd, NULL, TRUE);
+            
+
+        }
+        else if (LOWORD(wParam) == 2) { //2번 
+            drawmode = 2;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        else if (LOWORD(wParam) == 3) { //3번 
+            drawmode = 3;
+            bonoVisible = true;
+            SetFocus(hWnd);
+            InvalidateRect(hWnd, NULL, TRUE);
+            
+        }
+        else if (LOWORD(wParam) == 4) { //4번 
+            drawmode = 4;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        else if (LOWORD(wParam) == 5) { //5번 
+            drawmode = 5;
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+    case WM_MOUSEMOVE:
+
+        POINT mousePos;
+        GetCursorPos(&mousePos);
+
+        // 이동한 마우스 위치가 드로잉 영역 내에 있는지 확인합니다.
+        if (mousePos.x >= drawstart.x && mousePos.x <= drawend.x &&
+            mousePos.y >= drawstart.y && mousePos.y <= drawend.y) {
+            mindraw = TRUE; // 드로잉 영역 내에 마우스가 있음
+        }
+        else {
+            mindraw = FALSE; // 드로잉 영역 밖에 마우스가 있음
+        }
+        break;
+         
+    case WM_PAINT:
+        if (true) {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            // 클라이언트 영역 크기 가져오기
+            RECT clientRect;
+            GetClientRect(hWnd, &clientRect);
+
+            int margin = 8;
+            int boxWidth = clientRect.right - clientRect.left - 2 * margin;
+            int boxHeight = clientRect.bottom - clientRect.top - 2 * margin;
+
+            // RGB(255, 240, 200) 색상의 네모 그리기
+            HBRUSH hColorBrush = CreateSolidBrush(RGB(255, 240, 200)); // 원하는 색상의 브러시 생성
+            SelectObject(hdc, hColorBrush);
+
+            int left = margin;
+            int top = margin;
+            int right = left + boxWidth;
+            int bottom = top + boxHeight;
+
+            //box그리기 
+            Rectangle(hdc, left, top, right, bottom);
+
+            //드로잉영역그리기 
+            HBRUSH whiteBrush = CreateSolidBrush(RGB(255, 255, 255));
+            SelectObject(hdc, whiteBrush);
+            Rectangle(hdc, left + 8, top + 88, right - 8, bottom - 8);
+            drawstart.x = left + 8;
+            drawstart.y = top + 88;
+            drawend.x = right - 8;
+            drawend.y = bottom - 8;
+            if (wParam == VK_SPACE) {
+                sp = true;
+            }
+
+            bono(hWnd, hdc,sp);
+            EndPaint(hWnd, &ps);
+
+        }
+
+
+        break;
+    case WM_SETCURSOR: {
+        POINT pt;
+        GetCursorPos(&pt);
+        ScreenToClient(hWnd, &pt);
+
+        if (pt.x > drawstart.x && pt.x < drawend.x &&
+            pt.y > drawstart.y && pt.y < drawend.y) {
+            // 마우스 커서를 십자가 모양으로 설정
+            SetCursor(LoadCursor(NULL, IDC_CROSS));
+            return TRUE; // 처리됨을 알림
+        }
+        else { //다시 화살표 
+            SetCursor(LoadCursor(NULL, IDC_ARROW));
+        }
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
+
+    HWND hWnd;
+    HWND hButton1, hButton2, hButton3, hButton4, hButton5;
+
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = CreateSolidBrush(RGB(255, 240, 200)); // 배경색 설정
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"ButtonWindowClass";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        return 1;
+    }
+
+    // 윈도우 창 크기를 800x480로 설정하고 메뉴바를 포함하지 않도록 스타일을 조절합니다.
+    hWnd = CreateWindow(
+        L"ButtonWindowClass", L"Win32 Button Example", WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX,
+        CW_USEDEFAULT, 0, 800, 480, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd) {
+        return FALSE;
+    }
+    int btnmargin = 146; //버튼+ 마진 
+    int btnstart = 32;// 1번 버튼 시작지점 
+    hButton1 = CreateWindow(
+        L"BUTTON", L"Box", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        btnstart, 16, 130, 64, hWnd, (HMENU)1, hInstance, NULL);
+
+    hButton2 = CreateWindow(
+        L"BUTTON", L"Circle", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        btnstart + btnmargin, 16, 130, 64, hWnd, (HMENU)2, hInstance, NULL);
+
+    hButton3 = CreateWindow(
+        L"BUTTON", L"Bonobono", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        btnstart + btnmargin * 2, 16, 130, 64, hWnd, (HMENU)3, hInstance, NULL);
+
+    hButton4 = CreateWindow(
+        L"BUTTON", L"Ryan", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        btnstart + btnmargin * 3, 16, 130, 64, hWnd, (HMENU)4, hInstance, NULL);
+    hButton5 = CreateWindow(
+        L"BUTTON", L"Cube", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+        btnstart + btnmargin * 4, 16, 130, 64, hWnd, (HMENU)5, hInstance, NULL);
+
+
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
+}
